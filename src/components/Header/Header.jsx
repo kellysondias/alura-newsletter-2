e92d752bde930c2665ad59713081512e19ddf6b2
Kellyson Dias <77993169+kellysondias@users.noreply.github.com>
2023-03-07 14:33:44 -0300

    Update "Header.jsx"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { ToggleTheme } from "../ToggleTheme/ToggleTheme";
 
-export const Header = ({ user }) => {
-	return (
-		<div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-			<span className="text-gray-100">
-				{user && `Olá, ${user?.name}!`}
-			</span>
-			<h1 className="">Alura Newsletter</h1>
-			<ToggleTheme />
-		</div>
-	);
-};
+export const Header = ({ user }) => (
+	<div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+		<span className="text-gray-100">
+			{user && `Olá, ${user?.name}!`}
+		</span>
+		<h1 className="">Alura Newsletter</h1>
+		<ToggleTheme />
+	</div>
+	
+);
